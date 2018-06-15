b8974f4e36b79ab7504051ae95472266e5036fcb
Christian Wolfram <c.wolfram@chriwo.de>
2018-06-15 21:20:55 +0200

    [!!!][TASK] Add initialize file after first ddev start into project
    The way to save an initialize file into the docker container are not
    working after ddev update to version 0.19.0. Now the file would be
    located in the build-folder.
    
    If you need fresh a fresh installation of your project after the first
    start of ddev, remove the initialize file and run `ddev remove -R` to
    delete all container and start with a fresh installation.

--- build/initialize/project_ddev_startup.sh
+++ build/initialize/project_ddev_startup.sh
@@ -1,13 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
 # set ssh keys
 /var/www/html/build/initialize/project_ddev_ssh.sh
 
-if [ -e /tmp/initialized ]
+if [ ! -e /var/www/html/build/initialized ]
 then
-    echo "Already initialized"
-else
-    echo "Initializing"
+    echo -e "\033[1;33m -- Initializing -- \033[0m"
+    touch /var/www/html/build/initialized
     /var/www/html/build/initialize/project_ddev_initialize.sh
-    touch /tmp/initialized
+else
+    echo -e "\033[32m -- Nothing to do, all is fine and ready -- \033[0m"
 fi
