63e6a55a98805c9b3aac87f5242ce93ce01887d2
Christian Wolfram <c.wolfram@chriwo.de>
2018-10-22 21:46:59 +0200

    [!!!][TASK][CLEANUP] Consider only id_rsa file as public key

--- build/initialize/project_ddev_ssh.sh
+++ build/initialize/project_ddev_ssh.sh
@@ -10,11 +10,6 @@ pushd "${PROJECT_DIRECTORY}"
 rm -rf /home/.ssh
 mkdir /home/.ssh
 
-if [ -f /tmp/.ssh/ddev.pub ]
-then
-    cp -f /tmp/.ssh/{ddev,ddev.pub,known_hosts} /home/.ssh/
-    mv /home/.ssh/ddev.pub /home/.ssh/id_rsa.pub && mv /home/.ssh/ddev /home/.ssh/id_rsa
-else
-    cp -f /tmp/.ssh/{id_rsa,id_rsa.pub,known_hosts} /home/.ssh/
-fi
+cp -f /tmp/.ssh/{id_rsa,id_rsa.pub,known_hosts} /home/.ssh/
+
 chmod 600 /home/.ssh/*
